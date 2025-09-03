a3c952b3e2d9a209e5aae506a52b388174a65b96
minkyong00 <minkyong131@gmail.com>
2025-09-03 17:03:47 +0900

    stockCalendarApp.js, stockCalendarState.js에 주석 추가, 배당락일 캘린더에 표시하는 css 수정

--- src/main/webapp/js/stockCalendarApp.js
+++ src/main/webapp/js/stockCalendarApp.js
@@ -4,14 +4,12 @@
 
 initCalendar();
 
+// 왼쪽, 오른쪽 화살표 클릭 시 moveCalendar 함수 호출
 document.querySelector("#leftArrow").addEventListener("click", () => moveCalendar(-1));
 document.querySelector("#rightArrow").addEventListener("click", () => moveCalendar(1));
   
+// 검색창
 const stockInput = document.querySelector("#search-input");
-
-stockInput.addEventListener("input", e => {
-	exdateCalendarState.stock.keyword = e.target.value;
-});
 
 stockInput.addEventListener("keyup", e => {
 	
@@ -19,42 +17,47 @@ stockInput.addEventListener("keyup", e => {
 	document.querySelector("#autocompleteContainer").innerHTML = "";
 	
 	if(keyword.length > 0){
+		// 입력창과 같은 주식 종목명 자동완성 리스트 만드는 함수
 		createInputSearchAutocompleteWindow(exdateCalendarState, keyword);
 	}
-	
-   //if (e.key === "Enter") {getStockDivied();}
    }
 ); 
   
 /**
  * 배당락일 API 호출
  */
-const getStockDivied = async (name) => {
+const getStockDiviedAPI = async (name) => {
   try {
-    exdateCalendarState.ui.loading = true;
     const response = await fetch(
       `http://localhost:8888/stockCalendar.stockwave?itmsName=${name}`
     );
     const data = await response.json();
 
     if (data && data.length > 0) {
-      exdateCalendarState.calendar.stockDiviedListSorted = data.sort().reverse().map(d => ({
-        year: Number(d.substring(0, 4)),
-        month: Number(d.substring(4, 6)) - 1,
-        day: Number(d.substring(6, 8))
-      }));
+	  // 가장 가까운 날짜 순으로 정렬
+      exdateCalendarState.calendar.stockDiviedListSorted 
+      	= data.sort().reverse().map(d => ({
+			// year, month, day로 객체 매핑
+        	year: Number(d.substring(0, 4)),
+        	month: Number(d.substring(4, 6)) - 1,
+        	day: Number(d.substring(6, 8))
+      	}));
 
+	  // 최근 배당락일 하나만 가져와 firstDate에 담음
       const firstDate = exdateCalendarState.calendar.stockDiviedListSorted[0];
+      // 상태 객체 year, month에 yyyy, mm를 담음
       exdateCalendarState.calendar.currentYear = firstDate.year;
       exdateCalendarState.calendar.currentMonth = firstDate.month;
+      // today에는 최근 배당락일 yyyymmdd를 넣음
       exdateCalendarState.calendar.today = firstDate;
 
-      // 같은 연도 배당락일만 리스트에 표시
-      exdateCalendarState.stock.diviList = exdateCalendarState.calendar.stockDiviedListSorted.filter(
-        d => d.year === firstDate.year
+      // 같은 연도 배당락일만 상태 객체 diviList에 담음
+      exdateCalendarState.stock.diviList 
+      	= exdateCalendarState.calendar.stockDiviedListSorted.filter(
+        	d => d.year === firstDate.year
       );
 
-      // 선택 가능한 년도 Set
+      // 선택 가능한 년도 Set(중복없앰)
       exdateCalendarState.stock.years = [...new Set(exdateCalendarState.calendar.stockDiviedListSorted.map(d => d.year))];
 
       // 렌더링
@@ -65,11 +68,10 @@ const getStockDivied = async (name) => {
 
   } catch (err) {
     console.error(err);
-  } finally {
-    exdateCalendarState.ui.loading = false;
   }
 }; 
 
+/* stock json */
 const getStockJsonApi = async () => {
 	try{
 		const response = await fetch(
@@ -79,6 +81,7 @@ const getStockJsonApi = async () => {
     	
     	if(data&&data.length>0){
 			data.forEach(stock => {
+				// 주식 종목명만 names 상태객체 리스트에 넣음
 				exdateCalendarState.stock.names.push(stock.name);
 			})
 		}
--- src/main/webapp/js/stockCalendarState.js
+++ src/main/webapp/js/stockCalendarState.js
@@ -2,21 +2,18 @@
  * 상태 객체, 함수
  */
 
+// 배당락일 캘린더 상태 객체
 const exdateCalendarState = {
   calendar: {
-    currentYear: null,
-    currentMonth: null,
+    currentYear: null, // 현재 년도
+    currentMonth: null, // 현재 월
     today: null, // 실제 오늘 or 최근 배당락일
-    stockDiviedListSorted: []
+    stockDiviedListSorted: [] // 배당락일 리스트
   },
   stock: {
-    keyword: null, // 검색한 종목명
     diviList: [], // 특정 연도 배당락일 리스트
     years: [], // 선택가능한 연도 목록
     names: [] // 종목명 목록
-  },
-  ui: {
-    loading: false, // API 로딩 상태
   }
 };
 
@@ -24,15 +21,19 @@ const exdateCalendarState = {
  * 달력 이동
  */
 const moveCalendar = (offset) => {
+  // 새로운 date 객체 생성
+  // 상태 객체의 년도와 월을 가져와 월은 파라미터에 따라 값이 달라짐
   const newDate = new Date(
     exdateCalendarState.calendar.currentYear,
     exdateCalendarState.calendar.currentMonth + offset,
     1
   );
 
+  // 상태 객체 year, month를 위에서 만든 date로 넣음
   exdateCalendarState.calendar.currentYear = newDate.getFullYear();
   exdateCalendarState.calendar.currentMonth = newDate.getMonth();
 
+  // 달력 렌더링
   renderCalendar();
 };
 
@@ -41,21 +42,27 @@ const moveCalendar = (offset) => {
  * 달력 그리기
  */
 const drawCalendar = (year, month, stockDiviedListSorted = [], today = null) => {
+  
   const calendar = document.getElementById("calendar");
-  calendar.innerHTML = "";
+  calendar.innerHTML = ""; // 초기화
 
   const table = document.createElement("table");
 
-  const date = new Date();
-  const currentDay = today ? today.day : date.getDate();
-
+  // 파라미터 년도, 월, 1일의 date 객체 생성
   const theDate = new Date(year, month, 1);
+  // 1일의 요일
   const theDay = theDate.getDay();
 
+  // 월의 마지막 날짜
   const last = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  
+  // 윤달인 경우 마지막 날짜를 29로 함
   if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) last[1] = 29;
+  
+  // 파라미터 월의 마지막 날짜
   const lastDay = last[month];
-
+  
+  // 캘린더 행 수 계산
   const row = Math.ceil((theDay + lastDay) / 7);
 
   table.innerHTML = `
@@ -65,45 +72,55 @@ const drawCalendar = (year, month, stockDiviedListSorted = [], today = null) => 
     </tr>
   `;
 
+  // 1일부터 시작
   let dNum = 1;
 
-  for (let i = 1; i <= row; i++) {
+  for (let i = 1; i <= row; i++) { // 행만큼 tr 생성
     const tr = document.createElement("tr");
     table.appendChild(tr);
-
-    for (let k = 0; k < 7; k++) {
+    
+    for (let k = 0; k < 7; k++) { // 7일 td 생성
       let td = document.createElement("td");
-
+      td.className = "calendar-td";
+      
+      // 첫째주이고, 1일의 요일이 k보다 작거나 
+      // 날짜가 월의 마지막 날짜보다 클 경우
       if ((i === 1 && k < theDay) || dNum > lastDay) {
-        td.innerHTML = "&nbsp;";
+        td.innerHTML = "&nbsp;"; // td에 공백 추가
       } else {
-        td.innerHTML = dNum;
+        td.innerHTML = dNum; // 아니면 td에 날짜 추가
 
-        // 오늘 표시
-        if (today && dNum === today.day && month === today.month && year === today.year) {
+        // td id에 오늘 표시
+        if (today && dNum === today.day 
+        	&& month === today.month && year === today.year) {
           td.id = "today";
         }
 
         // 배당락일 표시
         for (let date of stockDiviedListSorted) {
-          if (dNum === date.day && month === date.month && year === date.year) {
-            td.style.backgroundColor = "yellow";
+          if (dNum === date.day && month === date.month 
+          		&& year === date.year) {
+			const div = document.createElement("div");
+			div.id = "exdate";
+			const p = document.createElement("p");
+			p.innerHTML = dNum;
+			div.appendChild(p);
+			td.append(div);
           }
         }
 
-        dNum++;
+        dNum++; // 일 증가
       }
-
       tr.appendChild(td);
     }
   }
-
   calendar.appendChild(table);
 };
 
 /**
  * 렌더링 함수
  */
+// 캘린더 렌더링
 const renderCalendar = () => {
   drawCalendar(
     exdateCalendarState.calendar.currentYear,
@@ -118,10 +135,12 @@ const renderCalendar = () => {
   );
 };
 
+// 배당락일 리스트 렌더링
 const renderStockList = () => {
   getDiviList(exdateCalendarState.calendar.currentYear, exdateCalendarState.calendar.stockDiviedListSorted);
 };
 
+// 년도 필터 select 렌더링
 const renderYearFilter = () => {
   filterStockYear(exdateCalendarState.calendar.stockDiviedListSorted);
 };
@@ -131,13 +150,16 @@ const renderYearFilter = () => {
  */
 function getDiviList(year, stockDiviedListSorted) {
   const container = document.querySelector("#dividendList-list");
-  container.innerHTML = "";
+  container.innerHTML = ""; // 초기화
 
   const ul = document.createElement("ul");
   ul.id = "stockDiviUl";
 
   stockDiviedListSorted.forEach(date => {
-    if (date.year === year) {
+	  
+	// api의 배당락일의 년도와 캘린더의 년도가 같을 경우
+	// li 추가
+    if (date.year === year) { 
       const li = document.createElement("li");
       li.className = "stockDiviLi";
       li.textContent = `${date.year}-${String(date.month + 1).padStart(2, "0")}-${String(date.day).padStart(2, "0")}`;
@@ -147,6 +169,8 @@ function getDiviList(year, stockDiviedListSorted) {
 
   container.appendChild(ul);
 
+  // 배당락일 날짜 li 클릭 시 
+  // 해당 캘린더로 이동하는 함수 호출
   stockMoveCalendar(); 
 }
 
@@ -154,6 +178,7 @@ function getDiviList(year, stockDiviedListSorted) {
  * 캘린더 타이틀 생성
  */
 function createCalendarTitle(year, month) {
+  // 캘린더의 타이틀인 년도와 월 추가
   const dateContainer = document.querySelector(".date");
   dateContainer.innerHTML = "";
   const p = document.createElement("p");
@@ -169,8 +194,10 @@ function filterStockYear(stockDiviedListSorted) {
   const select = document.getElementById("yearSelect");
   select.innerHTML = "";
 
+  // api에서 년도 중복 set으로 없앰
   const stockYearList = [...new Set(stockDiviedListSorted.map(d => d.year))];
-
+  
+  // select 자식요소로 option 태그들 생성 후 추가
   stockYearList.forEach(year => {
     const option = document.createElement("option");
     option.value = year;
@@ -178,6 +205,7 @@ function filterStockYear(stockDiviedListSorted) {
     select.appendChild(option);
   });
 
+  // select의 년도 클릭 시 아래 해당 년도의 배당락일 리스트 보여줌
   select.addEventListener("change", e => {
     const year = Number(e.target.value);
     getDiviList(year, stockDiviedListSorted);
@@ -188,15 +216,20 @@ function filterStockYear(stockDiviedListSorted) {
  * 배당락일 클릭 시 해당 날짜로 이동
  */
 function stockMoveCalendar() {
+  // 배당락일의 li를 list로 불러와서 
   document.querySelectorAll(".stockDiviLi").forEach(li => {
+	// li 클릭 시 
     li.addEventListener("click", () => {
+	  // - 단위로 split한 후 숫자로 매핑 후 구조분해 할당함
       const [year, month, day] = li.textContent.split("-").map(Number);
+      // 상태객체 today에 담기 위해 객체 생성
       const dateObj = { year, month: month - 1, day };
 
       exdateCalendarState.calendar.currentYear = year;
       exdateCalendarState.calendar.currentMonth = month - 1;
       exdateCalendarState.calendar.today = dateObj;
-
+      
+      // 캘린더 렌더링
       renderCalendar();
     });
   });
@@ -210,18 +243,25 @@ function createInputSearchAutocompleteWindow(exdateCalendarState, keyword){
 	const ul = document.createElement("ul");
 	ul.id = "autocomplete-ul";
 	
+	// 상태 객체의 name배열에서
 	exdateCalendarState.stock.names
+		// name에 input에 입력한 값이 포함되는 종목명만 걸러서
 		.filter(name => name.includes(keyword))
 		.forEach(name => {
+			// li에 추가함
 			const li = document.createElement("li");
 			li.className = "autocomplete-li";
 			li.innerHTML = `${name}`;
 			
+			// li 클릭하면 
 			li.addEventListener("click", () => {
+				// input의 value에 name이 들어가고
 				document.querySelector("#search-input").value = name;
+				// 자동완성 ul 안보임
 				ul.style.display = "none";
 				
-          		getStockDivied(name);
+				// 클릭한 종목명으로 배당락일 api 호출함
+          		getStockDiviedAPI(name);
 			})
 			
 			ul.append(li);
@@ -231,10 +271,14 @@ function createInputSearchAutocompleteWindow(exdateCalendarState, keyword){
 }
 
 /**
- * 초기화
+ * 처음 로딩 시 나오는 캘린더
  */
 function initCalendar() {
+	
+  // 오늘 날짜 객체 생성
   const today = new Date();
+  
+  // 상태 객체에 오늘 년도, 월, 일 넣음
   exdateCalendarState.calendar.currentYear = today.getFullYear();
   exdateCalendarState.calendar.currentMonth = today.getMonth();
   exdateCalendarState.calendar.today = {
@@ -243,6 +287,7 @@ function initCalendar() {
     day: today.getDate()
   };
 
+  // 캘린더 렌더링
   renderCalendar();
 
 }
